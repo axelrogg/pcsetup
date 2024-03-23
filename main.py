63439850c96cd785c8013594b0fbdffc2afecc57
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-03-23 23:23:36 +0100

    let's test installing neovim

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ HOME_PATH = Path.home()
 def exec(cmd: str, capture_output: bool = True) -> subprocess.CompletedProcess:
     return subprocess.run(shlex.split(cmd), capture_output=capture_output)
 
+def append_to_profile():
+    screen = """#Added for my very old slot machine monitor
+xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync
+xrandr --addmode DP-1 "1920x1080_60.00"
+xrandr --addmode eDP-1 "1920x1080_60.00"
+"""
+
 
 def gitClone(repo: str, saveTo: Path):
     print(f"Cloning repo {repo} to {str(saveTo)}")
@@ -19,39 +26,48 @@ def gitClone(repo: str, saveTo: Path):
     print(f"Error while cloning repo: {clone_result.stderr.decode('utf-8')}")
     return False
 
+def add_apt_repos():
+    repos = [
+        "sudo apt-add-repository ppa:fish-shell/release-3 -y",
+    ]
+    for repo in repos:
+        result = exec(repo)
+        if result.returncode != 0:
+            print(result)
+            return False
+
 
 def add_apt_packages():
     pkgs = [
         "build-essential",
         "calibre",
+        "cmake",
         "curl",
+        "gettext",
         "htop",
         "libfuse2",
         "mmv",
+        "ninja-build",
         "nodejs",
         "npm",
+        "postgresql",
         "thunderbird",
         "tmux",
         "tree",
+        "unzip",
         "valgrind",
         "vlc",
     ]
-    print("Installing apt packages")
-    result = exec("apt-get install -y " + " ".join(pkgs))
-    if result.returncode == 0:
-        print("Installed apt packages")
-        return True
-    print(f"Error installing apt packages: {result.stderr.decode('utf-8')}")
-    return False
+    return exec("sudo apt-get install -y " + " ".join(pkgs))
 
 
 def install_neovim():
-
     repo_is_cloned = gitClone("https://github.com/neovim/neovim", HOME_PATH.joinpath("apps/neovim"))
     if not repo_is_cloned:
-        print("Could not install neovim")
         return False
-    print("Neovim was installed")
+    exec("cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo")
+    result = exec("sudo make install")
+    print(result)
     return True
 
 
@@ -70,14 +86,23 @@ set-option -g history-limit 5000
 # Change status bar color
 set -g default-terminal "screen-256color"
 set -g status-bg black
-set -g status-fg white"""
+set -g status-fg white
+"""
 
-    with open(HOME_PATH.joinpath(".tmux.conf"), "w", encoding="utf-8") as tmux_file:
-        tmux_file.write(content)
+    with open(HOME_PATH.joinpath(".tmux.conf"), "w", encoding="utf-8") as file:
+        file.write(content)
 
 
 def main():
-    add_apt_packages()
+    print("Installing apt packages")
+    result = add_apt_packages()
+    print(result)
+    if result.returncode == 0:
+        print("All apt packages were installed")
+    else:
+        print(result)
+
+    print("Installing Neovim")
     install_neovim()
 
 
